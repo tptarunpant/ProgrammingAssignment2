fc3689812d1d3dd2e1458df1e35f02caae84fd42
tptarunpant <tptarunpant@gmail.com>
2020-07-05 14:37:34 +0530

    Added comments within function bodies

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
-        set <- function(y) {
+        set <- function(y) { #Function to set the value of matrix in a non-local environment variable
                 x <<- y
                 i <<- NULL
         }
-        get <- function() x
-        setinv <- function(inv) i <<- inv
-        getinv <- function() i
-        list(set = set, get = get,
+        get <- function() x #Function to get the value of matrix from a non-local environment variable
+        setinv <- function(inv) i <<- inv #Function to set the inverse value of matrix 
+        getinv <- function() i #Function to get the inverse value of matrix 
+        list(set = set, get = get, #Returns list of functions
              setinv = setinv,
              getinv = getinv)
 }
@@ -23,13 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinv()
+        i <- x$getinv()#Get the inverse value of matrix
         if(!is.null(i)) {
                 message("getting cached data")
-                return(i)
+                return(i) #Return the inverse of matrix from the cache given it is not already cacluated
         }
         data <- x$get()
-        i <- solve(data, ...)
+        i <- solve(data, ...) #Calculate the inverse of the matrix using the solve function
         x$setinv(i)
         i
 }
